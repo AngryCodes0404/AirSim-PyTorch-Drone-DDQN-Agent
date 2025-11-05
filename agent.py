ac986a528563a54948f2a21a5e2d14ae0c076c22
AngryCodes0404 <angrybird.codes@gmail.com>
2025-11-05 08:21:10 -0500

    create dev branch

--- agent.py
+++ agent.py
@@ -18,7 +18,8 @@ torch.manual_seed(0)
 random.seed(0)
 np.random.seed(0)
 
-writer = SummaryWriter()  #"runs/Mar03_14-55-58_DESKTOP-QGNSALL"
+writer = SummaryWriter()  # "runs/Mar03_14-55-58_DESKTOP-QGNSALL"
+
 
 class DQN(nn.Module):
     def __init__(self, in_channels=1, num_actions=4):
@@ -26,7 +27,7 @@ class DQN(nn.Module):
         self.conv1 = nn.Conv2d(in_channels, 84, kernel_size=4, stride=4)
         self.conv2 = nn.Conv2d(84, 42, kernel_size=4, stride=2)
         self.conv3 = nn.Conv2d(42, 21, kernel_size=2, stride=2)
-        self.fc4 = nn.Linear(21*4*4, 168)
+        self.fc4 = nn.Linear(21 * 4 * 4, 168)
         self.fc5 = nn.Linear(168, num_actions)
 
     def forward(self, x):
@@ -54,18 +55,17 @@ class Agent:
         self.steps_done = 0
 
         if self.useGPU:
-            self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+            self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         else:
-            self.device = torch.device('cpu')
+            self.device = torch.device("cpu")
 
         self.dqn = DQN()
         self.env = DroneEnv(useGPU, useDepth)
         self.memory = deque(maxlen=10000)
         self.optimizer = optim.Adam(self.dqn.parameters(), self.learning_rate)
 
-
-        print('Using device:', self.device)
-        if self.device.type == 'cuda':
+        print("Using device:", self.device)
+        if self.device.type == "cuda":
             print(torch.cuda.get_device_name(0))
 
         # LOGGING
@@ -78,31 +78,36 @@ class Agent:
             self.dqn = self.dqn.to(self.device)  # to use GPU
 
         # model backup
-        files = glob.glob(self.save_dir + '\\*.pt')
+        files = glob.glob(self.save_dir + "\\*.pt")
         if len(files) > 0:
             files.sort(key=os.path.getmtime)
             file = files[-1]
             checkpoint = torch.load(file)
-            self.dqn.load_state_dict(checkpoint['state_dict'])
-            self.optimizer.load_state_dict(checkpoint['optimizer'])
-            self.episode = checkpoint['episode']
-            self.steps_done = checkpoint['steps_done']
-            print("Saved parameters loaded"
-                  "\nModel: ", file,
-                  "\nSteps done: ", self.steps_done,
-                  "\nEpisode: ", self.episode)
+            self.dqn.load_state_dict(checkpoint["state_dict"])
+            self.optimizer.load_state_dict(checkpoint["optimizer"])
+            self.episode = checkpoint["episode"]
+            self.steps_done = checkpoint["steps_done"]
+            print(
+                "Saved parameters loaded" "\nModel: ",
+                file,
+                "\nSteps done: ",
+                self.steps_done,
+                "\nEpisode: ",
+                self.episode,
+            )
 
         else:
             if os.path.exists("log.txt"):
-                open('log.txt', 'w').close()
+                open("log.txt", "w").close()
             if os.path.exists("last_episode.txt"):
-                open('last_episode.txt', 'w').close()
+                open("last_episode.txt", "w").close()
             if os.path.exists("last_episode.txt"):
-                open('saved_model_params.txt', 'w').close()
+                open("saved_model_params.txt", "w").close()
 
         obs = self.env.reset()
         tensor = self.transformToTensor(obs)
         writer.add_graph(self.dqn, tensor)
+
     def transformToTensor(self, img):
         if self.useGPU:
             tensor = torch.cuda.FloatTensor(img)
@@ -128,7 +133,7 @@ class Agent:
         )
         self.steps_done += 1
         if random.random() > self.eps_threshold:
-            #print("greedy")
+            # print("greedy")
             if self.useGPU:
                 action = np.argmax(self.dqn(state).cpu().data.squeeze().numpy())
                 return int(action)
@@ -146,7 +151,11 @@ class Agent:
             (
                 state,
                 action,
-                torch.cuda.FloatTensor([reward]) if self.useGPU else torch.FloatTensor([reward]),
+                (
+                    torch.cuda.FloatTensor([reward])
+                    if self.useGPU
+                    else torch.FloatTensor([reward])
+                ),
                 self.transformToTensor(next_state),
             )
         )
@@ -165,9 +174,15 @@ class Agent:
 
         if self.useGPU:
             next_q_values = self.dqn(next_states).cpu().detach().numpy()
-            max_next_q = torch.cuda.FloatTensor(next_q_values[[range(0, self.batch_size)], [actions]])
-            current_q = torch.cuda.FloatTensor(self.dqn(states)[[range(0, self.batch_size)], [actions]])
-            expected_q = rewards.to(self.device) + (self.gamma * max_next_q).to(self.device)
+            max_next_q = torch.cuda.FloatTensor(
+                next_q_values[[range(0, self.batch_size)], [actions]]
+            )
+            current_q = torch.cuda.FloatTensor(
+                self.dqn(states)[[range(0, self.batch_size)], [actions]]
+            )
+            expected_q = rewards.to(self.device) + (self.gamma * max_next_q).to(
+                self.device
+            )
         else:
             next_q_values = self.dqn(next_states).detach().numpy()
             max_next_q = next_q_values[[range(0, self.batch_size)], [actions]]
@@ -204,43 +219,102 @@ class Agent:
                 steps += 1
                 score += reward
                 if done:
-                    print("----------------------------------------------------------------------------------------")
-                    print("episode:{0}, reward: {1}, mean reward: {2}, score: {3}, epsilon: {4}, total steps: {5}".format(self.episode, reward, round(score/steps, 2), score, self.eps_threshold, self.steps_done))
+                    print(
+                        "----------------------------------------------------------------------------------------"
+                    )
+                    print(
+                        "episode:{0}, reward: {1}, mean reward: {2}, score: {3}, epsilon: {4}, total steps: {5}".format(
+                            self.episode,
+                            reward,
+                            round(score / steps, 2),
+                            score,
+                            self.eps_threshold,
+                            self.steps_done,
+                        )
+                    )
                     score_history.append(score)
                     reward_history.append(reward)
-                    with open('log.txt', 'a') as file:
-                        file.write("episode:{0}, reward: {1}, mean reward: {2}, score: {3}, epsilon: {4}, total steps: {5}\n".format(self.episode, reward, round(score/steps, 2), score, self.eps_threshold, self.steps_done))
+                    with open("log.txt", "a") as file:
+                        file.write(
+                            "episode:{0}, reward: {1}, mean reward: {2}, score: {3}, epsilon: {4}, total steps: {5}\n".format(
+                                self.episode,
+                                reward,
+                                round(score / steps, 2),
+                                score,
+                                self.eps_threshold,
+                                self.steps_done,
+                            )
+                        )
 
                     if self.useGPU:
-                        print('Total Memory:', self.convert_size(torch.cuda.get_device_properties(0).total_memory))
-                        print('Allocated Memory:', self.convert_size(torch.cuda.memory_allocated(0)))
-                        print('Cached Memory:', self.convert_size(torch.cuda.memory_reserved(0)))
-                        print('Free Memory:', self.convert_size(torch.cuda.get_device_properties(0).total_memory - (torch.cuda.max_memory_allocated() + torch.cuda.max_memory_reserved())))
+                        print(
+                            "Total Memory:",
+                            self.convert_size(
+                                torch.cuda.get_device_properties(0).total_memory
+                            ),
+                        )
+                        print(
+                            "Allocated Memory:",
+                            self.convert_size(torch.cuda.memory_allocated(0)),
+                        )
+                        print(
+                            "Cached Memory:",
+                            self.convert_size(torch.cuda.memory_reserved(0)),
+                        )
+                        print(
+                            "Free Memory:",
+                            self.convert_size(
+                                torch.cuda.get_device_properties(0).total_memory
+                                - (
+                                    torch.cuda.max_memory_allocated()
+                                    + torch.cuda.max_memory_reserved()
+                                )
+                            ),
+                        )
 
                         # tensorboard --logdir=runs
-                        memory_usage_allocated = np.float64(round(torch.cuda.memory_allocated(0) / 1024 ** 3, 1))
-                        memory_usage_cached = np.float64(round(torch.cuda.memory_reserved(0) / 1024 ** 3, 1))
+                        memory_usage_allocated = np.float64(
+                            round(torch.cuda.memory_allocated(0) / 1024**3, 1)
+                        )
+                        memory_usage_cached = np.float64(
+                            round(torch.cuda.memory_reserved(0) / 1024**3, 1)
+                        )
 
-                        writer.add_scalar("memory_usage_allocated", memory_usage_allocated, self.episode)
-                        writer.add_scalar("memory_usage_cached", memory_usage_cached, self.episode)
+                        writer.add_scalar(
+                            "memory_usage_allocated",
+                            memory_usage_allocated,
+                            self.episode,
+                        )
+                        writer.add_scalar(
+                            "memory_usage_cached", memory_usage_cached, self.episode
+                        )
 
-                    writer.add_scalar('epsilon_value', self.eps_threshold, self.episode)
-                    writer.add_scalar('score', score, self.episode)
-                    writer.add_scalar('reward', reward, self.episode)
-                    writer.add_scalar('Total steps', self.steps_done, self.episode)
-                    writer.add_scalars('General Look', {'epsilon_value': self.eps_threshold,
-                                                    'score': score,
-                                                    'reward': reward}, self.episode)
+                    writer.add_scalar("epsilon_value", self.eps_threshold, self.episode)
+                    writer.add_scalar("score", score, self.episode)
+                    writer.add_scalar("reward", reward, self.episode)
+                    writer.add_scalar("Total steps", self.steps_done, self.episode)
+                    writer.add_scalars(
+                        "General Look",
+                        {
+                            "epsilon_value": self.eps_threshold,
+                            "score": score,
+                            "reward": reward,
+                        },
+                        self.episode,
+                    )
 
                     # save checkpoint
                     if self.episode % self.save_interval == 0:
                         checkpoint = {
-                            'episode': self.episode,
-                            'steps_done': self.steps_done,
-                            'state_dict': self.dqn.state_dict(),
-                            'optimizer': self.optimizer.state_dict()
+                            "episode": self.episode,
+                            "steps_done": self.steps_done,
+                            "state_dict": self.dqn.state_dict(),
+                            "optimizer": self.optimizer.state_dict(),
                         }
-                        torch.save(checkpoint, self.save_dir + '//EPISODE{}.pt'.format(self.episode))
+                        torch.save(
+                            checkpoint,
+                            self.save_dir + "//EPISODE{}.pt".format(self.episode),
+                        )
 
                     self.episode += 1
                     end = time.time()
